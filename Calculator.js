477e50559ecf256d04729f7fffb0fe1a647e0740
AM_00 <taole.chen@gmail.com>
2023-01-04 20:59:24 +0100

    basic functionality works. negative numbers as operands not implemented.

--- Calculator.js
+++ Calculator.js
@@ -1,12 +1,114 @@
 class Calculator {
-    constructor() {}
+
+    constructor() {
+        this.result = 0;
+        this.rawInput ='';
+    }
 
 
-    add(x, y) { return 3;}
-    subtract() {}
-    multiply() {}
-    divide() {}
 
+
+    processInput() {
+        if (this.rawInput === '') return null;
+
+        let input = this.rawInput;
+        let x = null;
+        let y = null;
+        let op = null;
+        let buffer = '';
+        
+
+        while (input.length > 0) {
+            const char = input[0];
+            
+            if (!Number.isNaN(parseInt(char)) || char === '.') {
+                buffer += char;
+                input = input.slice(1);
+                continue;
+            } 
+            if (Number.isNaN(parseInt(char)) && char !== '.'){
+                if (x === null) {
+                    op = char;
+                    x = parseInt(buffer);
+                    buffer = '';
+                    input = input.slice(1);
+                } 
+                else {
+                    y = parseInt(buffer);
+                    buffer='';
+                    break;
+                }
+            }
+        }
+
+        //there was only one operand, 
+        if (y === null && buffer !== '') {
+            y = parseInt(buffer);
+            buffer = '';
+        }
+
+        //input is invalid
+        if (x === null || op === null || y === null) {
+            return null;
+        }
+
+        this.rawInput = input;
+        console.log(this.rawInput);
+        return {x: x, y: y, op: op};
+    }
+
+    add(x, y) { 
+        if (Number.isNaN(x) || Number.isNaN(y)) return 'ERROR';
+        
+        this.result = x + y;
+        return this.result;
+    }
+    subtract(x, y) {
+        if (Number.isNaN(x) || Number.isNaN(y)) return 'ERROR';
+        
+        this.result = x - y;
+        return this.result;
+    }
+    multiply(x, y) {
+        if (Number.isNaN(x) || Number.isNaN(y)) return 'ERROR';
+
+        this.result = x * y;
+        return this.result;
+    }
+    divide(x, y) {
+        if (Number.isNaN(x) || Number.isNaN(y)) return 'ERROR';
+        if (y === 0) return 'DIV BY 0';
+        
+        this.result = x / y;
+        return this.result;
+    }
+
+    operate() {
+        const {x, y, op} = this.processInput();
+
+
+        switch(op) {
+            case '+':
+                this.result = this.add(x, y);
+                break;
+            case '-':
+                this.result = this.subtract(x,y);
+                break;
+            case '*':
+                this.result = this.multiply(x,y);
+                break;
+            case '/':
+                this.result = this.divide(x,y);
+            default:
+                this.result = 0;
+                alert ('Invalid Input');
+        }
+
+        if (this.rawInput !== '') {
+            let s = this.result.toString();
+            s += this.rawInput;
+            this.rawInput = s;
+        }
+        return this.result;
+    }
 }
-
-module.exports = Calculator;
---
+++ script.js
@@ -0,0 +1,112 @@
+const c = new Calculator();
+
+class Controller {
+
+    
+
+    constructor(){
+        this.model = new Calculator();
+        this.view = new View();
+
+        this.app = document.querySelector('#root');
+
+
+        //create display
+        this.displayContainer = document.createElement('div');
+        this.displayContainer.setAttribute('id', 'displayContainer');
+
+        this.topline = document.createElement('h2');
+        this.topline.setAttribute('id', 'topline');
+        this.topline.textContent = '';
+        this.bottomline = document.createElement('h1');
+        this.bottomline.setAttribute('id', 'bottomLine');
+        this.bottomline.textContent = '0';
+        this.displayContainer.append(this.topline, this.bottomline);
+
+        //create inputs
+        this.inputContainer = document.createElement('div');
+        this.inputContainer.setAttribute('id', 'inputContainer');
+        for (let i = 0; i < 10; i++) {
+            const b = document.createElement('button');
+            b.textContent = i.toString();
+            b.setAttribute('id', 'btn' + i.toString());
+            b.classList.add('button');
+            this.inputContainer.appendChild(b);
+            b.addEventListener('click', () => {
+                this.topline.textContent += i.toString();
+            });
+        }
+
+        //create buttons
+        //addition
+        this.btn_add = document.createElement('button');
+        this.btn_add.textContent = '+';
+        this.btn_add.setAttribute('id', 'btnPlus');
+        this.btn_add.classList.add('button');
+        this.inputContainer.appendChild(this.btn_add);
+        this.btn_add.addEventListener('click', (e) => {
+            this.topline.textContent += '+';
+        });
+
+        //subtraction
+        this.btn_minus = document.createElement('button');
+        this.btn_minus.textContent = '-';
+        this.btn_minus.setAttribute('id', 'btnMinus');
+        this.btn_minus.classList.add('button');
+        this.inputContainer.appendChild(this.btn_minus);
+        this.btn_minus.addEventListener('click', (e) => {
+            this.topline.textContent += '-';
+        });
+
+        //multiplication
+        this.btn_mult = document.createElement('button');
+        this.btn_mult.textContent = '*';
+        this.btn_mult.setAttribute('id', 'btnMult');
+        this.btn_mult.classList.add('button');
+        this.inputContainer.appendChild(this.btn_mult);
+        this.btn_mult.addEventListener('click', (e) => {
+            this.topline.textContent += '*';
+        });
+
+        //division
+        this.btn_div = document.createElement('button');
+        this.btn_div.textContent = '/';
+        this.btn_div.setAttribute('id', 'btnDiv');
+        this.btn_div.classList.add('button');
+        this.inputContainer.appendChild(this.btn_div);
+        this.btn_div.addEventListener('click', (e) => {
+            this.topline.textContent += '/';
+        });
+
+        //compute
+        this.btn_result = document.createElement('button');
+        this.btn_result.textContent = '=';
+        this.btn_result.setAttribute('id', 'btnResult');
+        this.btn_result.classList.add('button');
+        this.inputContainer.appendChild(this.btn_result);
+        this.btn_result.addEventListener('click', (e) => {
+            // this.topline.textContent += '+';
+            this.model.rawInput = this.topline.textContent;
+            this.model.operate();
+            this.bottomline.textContent = this.model.result;
+            this.topline.textContent = this.model.rawInput;
+        });
+
+        //clear
+        this.btn_clear = document.createElement('button');
+        this.btn_clear.textContent = 'C';
+        this.btn_clear.setAttribute('id', 'btnResult');
+        this.btn_clear.classList.add('button');
+        this.inputContainer.appendChild(this.btn_clear);
+        this.btn_clear.addEventListener('click', (e) => {
+            this.topline.textContent = '';
+            this.model.rawInput = '';
+        });
+
+
+        this.app.append(this.displayContainer, this.inputContainer);
+    }
+
+}
+
+const app = new Controller();
